2f64d3da93571a6618ca63380e7d4d09ca362a8f
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-02 20:14:24 +0430

    [ref]:Added Drawer

--- src/Pages/Admin/index.tsx
+++ src/Pages/Admin/index.tsx
@@ -1,18 +1,49 @@
-import { AppBar, Avatar, Toolbar, Typography } from "@mui/material"
-import { LogoutBtn } from "src/Components"
+import { AppBar, List, ListItem, Divider, Drawer, Toolbar, Typography, ListItemButton, ListItemText, Box, CssBaseline, ListItemIcon } from "@mui/material"
+import { LogoutBtn, NameAvatar } from "src/Components"
+import { Inventory } from '@mui/icons-material'
+
 
 export const PanelPage = () => {
   return (
-    <AppBar position="static" >
-      <Toolbar>
-        <Typography
-          variant="h6"
-          sx={{ flexGrow: 1 }}
-        >
-          Admin Panel
-        </Typography>
-        <LogoutBtn />
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" sx={
+        { zIndex: (theme) => theme.zIndex.drawer + 1 }
+      }>
+        <Toolbar>
+          <NameAvatar />
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1 }}
+          >
+            Admin Panel
+          </Typography>
+          <LogoutBtn />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Inventory sx={{ color: "gray" }} />
+              </ListItemIcon>
+              <ListItemText primary="Products" />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+
+        </List>
+      </Drawer>
+    </Box>
   )
 }
