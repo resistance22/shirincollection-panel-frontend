b1e76d047ded05b0547fc627b8cbf117fbedfb16
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-02 15:20:25 +0430

    [impl]:Added logout to useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,13 +1,14 @@
 import { useAppDispatch, useAppSelector } from 'src/Store/hooks'
 import { crudentials } from 'src/Store/auth/types'
-import { login, loginWithStorageToken } from 'src/Store/auth'
+import { login, loginWithStorageToken, authSlice } from 'src/Store/auth'
 
 const useAuth = () => {
   const stateVal = useAppSelector((state) => state.auth)
   const appDispatch = useAppDispatch()
   const disatchLogin = (crudentials: crudentials) => appDispatch(login(crudentials))
   const dispatchLoginWithToken = (token: string) => appDispatch(loginWithStorageToken(token))
-  return { stateVal, disatchLogin, dispatchLoginWithToken }
+  const dispatchLogout = () => appDispatch(authSlice.actions.logout)
+  return { stateVal, disatchLogin, dispatchLoginWithToken, dispatchLogout }
 }
 
 export default useAuth
