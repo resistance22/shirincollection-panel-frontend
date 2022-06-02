762e0f8cdd30077a9dabf1e17179d14a425a574a
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-02 15:43:31 +0430

    [impl]: Implemented NameAvatar

---
+++ src/Components/NameAvatar.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from "@mui/material"
+import { useAuth } from "src/hooks"
+
+
+const NameAvatar = () => {
+  const { stateVal } = useAuth()
+  return (
+    <Avatar>
+      `${stateVal.user?.firstName[0]} ${stateVal.user?.lastName[0]}`
+    </Avatar>
+  )
+}
+
+export default NameAvatar
