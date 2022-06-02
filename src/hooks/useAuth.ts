3daaf593544734140b7589b6187c845432ac6007
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-02 15:34:43 +0430

    [ref]:Added dispatchLogout to useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -7,7 +7,7 @@ const useAuth = () => {
   const appDispatch = useAppDispatch()
   const disatchLogin = (crudentials: crudentials) => appDispatch(login(crudentials))
   const dispatchLoginWithToken = (token: string) => appDispatch(loginWithStorageToken(token))
-  const dispatchLogout = () => appDispatch(authSlice.actions.logout)
+  const dispatchLogout = () => appDispatch(authSlice.actions.logout())
   return { stateVal, disatchLogin, dispatchLoginWithToken, dispatchLogout }
 }
 
