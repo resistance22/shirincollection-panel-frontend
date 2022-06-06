1593ad90fd7693abd05cd15c31d10813c4d9923a
Amin Foroutan <aminforoutan7@gmail.com>
2022-06-07 00:25:07 +0430

    [ref]:Updated types

--- src/Store/products/types.ts
+++ src/Store/products/types.ts
@@ -1,11 +1,27 @@
+export interface NewEntryItem {
+  Title: string,
+  defaultValue: number
+}
+
+export interface EntryItem extends NewEntryItem {
+  id: number,
+  createdAt: string,
+  updatedAt: string,
+  publishedAt: null,
+}
+
 export interface NewProduct {
   Title: string
-  Descriptoin: string,
-  entry_items: []
+  Description: string,
+  entry_items: NewEntryItem[]
 }
 
 export interface Product extends NewProduct {
-  id: number
+  id: number,
+  entry_items: EntryItem[],
+  createdAt: string,
+  updatedAt: string,
+  publishedAt: null,
 }
 
 export interface initialStateType {
