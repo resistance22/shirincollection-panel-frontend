a0a950bce214e3a54a87feac1625fab212eeacd9
Amin Foroutan <aminforoutan7@gmail.com>
2022-06-07 00:23:01 +0430

    [ref]:Removed mainBox

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,5 +1,4 @@
 export { default as LogoutBtn } from './LogoutBtn'
 export { default as NameAvatar } from './NameAvatar'
 export { default as ProtectedRoute } from './ProtectedRoute'
-export { default as MainBox } from './mainBox'
 export { default as Page } from './Page'
