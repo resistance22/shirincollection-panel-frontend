735d584dc16fba75b35d568cc3adcc7667579339
Amin Foroutan <aminforoutan7@gmail.com>
2022-06-07 01:04:31 +0430

    [feat]:Added disptchGetProducts to useProduct hook

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,12 +1,13 @@
 import { useAppDispatch, useAppSelector } from 'src/Store/hooks'
 import { NewProduct } from 'src/Store/products/types'
-import { createProduct } from 'src/Store/products'
+import { createProduct, getProducts } from 'src/Store/products'
 
 const useProduct = () => {
   const stateVal = useAppSelector((state) => state.products)
   const appDispatch = useAppDispatch()
   const dispatchCreateProduct = (newProduct: NewProduct) => appDispatch(createProduct(newProduct))
-  return { stateVal, dispatchCreateProduct }
+  const dispatchGetProducts = () => appDispatch(getProducts())
+  return { stateVal, dispatchCreateProduct, dispatchGetProducts }
 }
 
 export default useProduct
