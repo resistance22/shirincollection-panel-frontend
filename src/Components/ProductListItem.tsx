6ea6c95207daba54918620a9686071a9792b004e
Amin Foroutan <aminforoutan7@gmail.com>
2022-06-07 14:15:59 +0430

    [ref]:Added ID col

--- src/Components/ProductListItem.tsx
+++ src/Components/ProductListItem.tsx
@@ -3,17 +3,20 @@ import { FC } from 'react'
 import { Product } from 'src/Store/products/types'
 
 
-export const ProductListItem: FC<Product> = ({ Title, Percent, entry_items }) => {
+export const ProductListItem: FC<Product> = ({ Title, Percent, entry_items, id }) => {
   return (
     <Grid className='product-list-item' container spacing={0.5}>
-      <Grid item xs={6}>
+      <Grid item xs={1}>
+        {id}
+      </Grid>
+      <Grid item xs={5}>
         {Title}
       </Grid>
       <Grid item xs={4}>
         {`${Percent}%`}
       </Grid>
       <Grid item xs={2}>
-        {entry_items.length}
+        {entry_items?.length}
       </Grid>
     </Grid>
   )
