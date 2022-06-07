492edc937ecf35e1fc2f63b29f67925f1076e13a
Amin Foroutan <aminforoutan7@gmail.com>
2022-06-07 14:18:23 +0430

    [feat]:Added dispatchGetProducts,dispatchResetState

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,13 +1,14 @@
 import { useAppDispatch, useAppSelector } from 'src/Store/hooks'
 import { NewProduct } from 'src/Store/products/types'
-import { createProduct, getProducts } from 'src/Store/products'
+import { createProduct, getProducts, productsSlice } from 'src/Store/products'
 
 const useProduct = () => {
   const stateVal = useAppSelector((state) => state.products)
   const appDispatch = useAppDispatch()
   const dispatchCreateProduct = (newProduct: NewProduct) => appDispatch(createProduct(newProduct))
-  const dispatchGetProducts = () => appDispatch(getProducts())
-  return { stateVal, dispatchCreateProduct, dispatchGetProducts }
+  const dispatchGetProducts = (page: number) => appDispatch(getProducts(page))
+  const dispatchResetState = () => appDispatch(productsSlice.actions.reset())
+  return { stateVal, dispatchCreateProduct, dispatchGetProducts, dispatchResetState }
 }
 
 export default useProduct
