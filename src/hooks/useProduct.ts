e7d81df6ba3322a43b71a3cddbb421563a95d915
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-07 19:48:09 +0430

    [feat]:Added dispatchDeleteProduct to useProduct

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,14 +1,15 @@
 import { useAppDispatch, useAppSelector } from 'src/Store/hooks'
 import { NewProduct } from 'src/Store/products/types'
-import { createProduct, getProducts, productsSlice } from 'src/Store/products'
+import { createProduct, getProducts, deleteProduct, productsSlice } from 'src/Store/products'
 
 const useProduct = () => {
   const stateVal = useAppSelector((state) => state.products)
   const appDispatch = useAppDispatch()
   const dispatchCreateProduct = (newProduct: NewProduct) => appDispatch(createProduct(newProduct))
   const dispatchGetProducts = (page: number) => appDispatch(getProducts(page))
+  const dispatchDeleteProduct = (id: number) => appDispatch(deleteProduct(id))
   const dispatchResetState = () => appDispatch(productsSlice.actions.reset())
-  return { stateVal, dispatchCreateProduct, dispatchGetProducts, dispatchResetState }
+  return { stateVal, dispatchCreateProduct, dispatchGetProducts, dispatchResetState, dispatchDeleteProduct }
 }
 
 export default useProduct
