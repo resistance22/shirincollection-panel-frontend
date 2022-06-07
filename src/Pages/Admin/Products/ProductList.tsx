b9dbcad3fa8ee3f58d68238ba0c8434a938f032c
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-07 18:28:52 +0430

    [ref]:Added Action col

--- src/Pages/Admin/Products/ProductList.tsx
+++ src/Pages/Admin/Products/ProductList.tsx
@@ -20,14 +20,17 @@ export const ProductList = () => {
         <Grid item xs={1}>
           {"ID"}
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={4}>
           {"Product Title"}
         </Grid>
         <Grid item xs={4}>
           {"Profit Percent"}
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={1}>
           {"Costs length"}
+        </Grid>
+        <Grid item xs={2}>
+          {"Actions"}
         </Grid>
       </Grid>
       {stateVal.loading ? <Loading /> : Object.keys(stateVal.products).map((productID) => {
