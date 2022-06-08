d1b2983c45daf6689e3abd31d4212f34d76ae5e9
Amin Foroutann <aminforoutan7@gmail.com>
2022-06-08 16:50:45 +0430

    [feat]: Added loading for delete

--- src/Components/ProductListItem.tsx
+++ src/Components/ProductListItem.tsx
@@ -2,11 +2,17 @@ import { Grid, IconButton, Tooltip } from '@mui/material'
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { Product } from 'src/Store/products/types'
-import { Edit, Delete } from '@mui/icons-material'
+import { Edit } from '@mui/icons-material'
 import { DeleteProductBtn } from 'src/Components'
+import { useProduct } from 'src/hooks'
+
 export const ProductListItem: FC<Product> = ({ Title, Percent, entry_items, id }) => {
+  const { stateVal } = useProduct()
   return (
-    <Grid className='product-list-item' container spacing={0.5}>
+    <Grid className='product-list-item' container spacing={0.5} sx={{
+      opacity: stateVal.deleting == id ? 0.5 : 1,
+      pointerEvents: stateVal.deleting == id ? "none" : "auto"
+    }}>
       <Grid item xs={1}>
         {id}
       </Grid>
