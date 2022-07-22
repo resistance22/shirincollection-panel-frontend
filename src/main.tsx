b20a5b9f39cf2a9109de114ac1e887b62d79bbce
Amin Foroutan <aminforoutan7@gmail.com>
2022-07-22 19:12:10 +0430

    [feat]:Added QueryPriveder

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
+import {
+  QueryClient,
+  QueryClientProvider
+} from 'react-query'
 import AppStore from 'src/Store'
 import './index.css'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={AppStore}>
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </React.StrictMode>
   </Provider>
 
