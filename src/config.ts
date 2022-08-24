dfeccc9a299e1a13fa724bc8e8f103066077329e
Amin Foroutan <aminforoutan7@gmail.com>
2022-08-24 12:58:51 +0430

    [ref]:Added production config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,15 @@
-export default {
-  backend_url: 'http://localhost:1337'
+const generateConfig = () => {
+  if (import.meta.env.MODE == 'production') {
+    return {
+      backend_url: 'http://api.shirincollection.com'
+    }
+  } else {
+    return {
+      backend_url: 'http://localhost:1337'
+    }
+  }
 }
+
+const config = generateConfig()
+
+export default config
